f1df03cb08f250d3b06005f21302eafec16a6c0c
RoottenJoe <avila1053@gmail.com>
2017-06-20 11:37:33 -0500

    Update Excercise2.c
    Add a second output on gpio06 that be the opposite state of gpio05

--- Excercise2.c
+++ Excercise2.c
@@ -1,5 +1,7 @@
 /*Excercise 2: using 2 Raspberry´s gpio as inputs (gpio14 and gpio15)
-turn on a led connected on gpio05 only if gpio14=on and gpio15=off*/
+turn on a led connected on gpio05 only if gpio14=on and gpio15=off
+Add a second output on gpio06 that be the opposite state of gpio05 */
+
 
 #include<stdio.h>
 #include<pigpio.h>
@@ -11,12 +13,15 @@ int main()
 	gpioSetMode(14, PI_INPUT);
 	gpioSetMode(15, PI_INPUT);
 	gpioSetMode(05, PI_OUTPUT);
+	gpioSetMode(06, PI_OUTPUT);
 	
 	for(;;)
 	{ if((gpioRead(14))&&(!gpioRead(15)))
-		gpioWrite(05, 1);
+		{ gpioWrite(05, 1);
+		  gpioWrite(06, 0); }
 	else
-		gpioWrite(05, 0);
+		{ gpioWrite(05, 0);
+		  gpioWrite(06, 1); }
 	}
 	
 	gpioTerminate();
