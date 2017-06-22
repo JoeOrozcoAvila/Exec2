25c087ce7169425ebe28e8fa2486a108207d60a5
por-aca <por.aca@gmail.com>
2017-06-22 18:09:32 -0500

    Adding flash effect to output on gpio05

--- Excercise2.c
+++ Excercise2.c
@@ -1,8 +1,13 @@
 /*Excercise 2: using 2 Raspberry´s gpio as inputs (gpio14 and gpio15)
-turn on a led connected on gpio05 only if gpio14=on and gpio15=off*/
+turn on a led connected on gpio05 only if gpio14=on and gpio15=off
+* 
+* Compilation: gcc -o Excercise2 Excercise2.c -lpigpio -pthread
+* 
+*/
 
 #include<stdio.h>
 #include<pigpio.h>
+#include<time.h>
 
 int main()
 {
@@ -13,8 +18,12 @@ int main()
 	gpioSetMode(05, PI_OUTPUT);
 	
 	for(;;)
-	{ if((gpioRead(14))&&(!gpioRead(15)))
+	{ if((gpioRead(14))&&(!gpioRead(15))) {
 		gpioWrite(05, 1);
+		sleep(1);
+		gpioWrite(05, 0);
+		sleep(1);
+	}
 	else
 		gpioWrite(05, 0);
 	}
